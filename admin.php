ab2a52edc561bf85e80ac7dead44ea792fe77ff2
jean-derieppe <jean.derieppe@laplateforme.io>
2022-11-28 21:09:56 +0100

    page de l'Admin de module-connexion

---
+++ admin.php
@@ -0,0 +1,23 @@
+<html>
+ <head>
+ <html lang="fr">
+ <meta charset="utf-8">
+ <!-- importer le fichier de style -->
+ <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+ <title>Admin</title>
+ </head>
+
+    <!--
+                Cette page est accessible UNIQUEMENT pour l’utilisateur “admin”. Elle permet
+                de lister l’ensemble des informations des utilisateurs présents dans la base de
+                données.
+
+                                                                    -->
+
+<!-- include Header.php -->
+ <?php include("header.php") ?>
+
+
+
+<!-- Include Footer.php -->
+<?php include("footer.php") ?>
