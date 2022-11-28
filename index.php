6462edce367ba83bae9fa3ca7978e2ea2a324a7e
jean-derieppe <jean.derieppe@laplateforme.io>
2022-11-28 21:07:12 +0100

    Accueil du module-connexion

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    <title>Acceuil</title>
+</head>
+<body>
+    <!-- include Header.php -->
+    <?php include("header.php") ?>
+    
+
+    <!-- Partie visible sur la page Accueil -->
+    <h1>Hello Sir</h1>
+<div class="div1">
+    <h2>Bienvenue sur le site qui vous permet de faire un module de connexion</h2> 
+        
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae aliquam expedita sint ex. Hic dicta<br>
+        neque magni error praesentium ullam ipsa dolorem, quam iure nemo nostrum modi quod corrupti.
+        <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed error libero assumenda expedita dolores dolorem<br>
+        ipsam eius consequatur debitis, ullam quos suscipit doloribus reiciendis ipsum?
+        <br>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br>
+        Nam omnis voluptates repudiandae accusantium non, similique<br>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa
+    </p>
+</div>
+       
+</body>
+<!-- Include Footer.php -->
+<?php include("footer.php") ?>
+
+</html>
