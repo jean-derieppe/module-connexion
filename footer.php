22eeea2142b4a627a65db909ede05a1e57c402bf
jean-derieppe <jean.derieppe@laplateforme.io>
2022-11-28 21:05:02 +0100

    footer du module-connexion

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer> 
+<nav class="footer">
+        <ul class="list1">
+            <a href="index.php">
+            <button>
+            <li>Accueil</li>
+            </button></a>
+
+            <a href="connexion.php">
+            <button>
+            <li>Connexion</li>
+            </button></a>
+
+            <a href="inscription.php">
+            <button>
+            <li >Inscription</li>
+            </button></a>
+
+            <a href="https://github.com/jean-derieppe">
+            <button>
+            <li >Lien Vers Git</li>
+            </button></a>
+
+            <a href="contact.php">
+            <button>
+            <li >Contact</li>
+            </button></a>
+
+            <a href="#">
+            <button>
+            <li>Haut-de-page</li>
+            </button></a>
+
+        </ul>
+    </nav>
+</footer>
