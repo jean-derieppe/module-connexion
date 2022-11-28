070fa9bb5ab1c0fb2ccf7cbd0b16848734c109a9
jean-derieppe <jean.derieppe@laplateforme.io>
2022-11-28 21:05:16 +0100

    header du module-connexion

---
+++ header.php
@@ -0,0 +1,39 @@
+<header>
+<div class="header">
+   <h1 class="Module">Module de connexion</h1>
+
+</div>
+    <nav>
+        <ul class="list1">
+            <a href="index.php">
+            <button>
+            <li>Accueil</li>
+            </button></a>
+
+            <a href="connexion.php">
+            <button>
+            <li>Connexion</li>
+            </button></a>
+
+            <a href="inscription.php">
+            <button>
+            <li >Inscription</li>
+            </button></a>
+
+            <a href="profil.php">
+            <button>
+            <li >Profil</li>
+            </button></a>
+
+            <a href="Contact.php">
+            <button>
+            <li>Contact</li>
+            </button></a>
+
+            <a href="deconnection.php">
+            <button>
+            <li>Déconnection</li>
+            </button></a>
+        </ul>
+    </nav>
+</header>
