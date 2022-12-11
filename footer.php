9d0d560547161abf4c12c05e81834c8e89f32e14
jean-derieppe <jean.derieppe@laplateforme.io>
2022-12-11 12:24:35 +0100

    footer ok

--- footer.php
+++ footer.php
@@ -3,16 +3,16 @@
     <nav class="footer">
         <ul class="list2">
 
-            <a class="pix" href="index.php">
-            <img src="House.png" title="Accueil" height="110px" width="110px">
+            <a href="index.php">
+            <img class="pix" src="House.png" title="Accueil" height="110px" width="110px">
             </a>
 
-            <a class="pix" href="#">
-            <img src="Haut.png" title="Haut de page" height="110px" width="110px">
+            <a href="#">
+            <img class="pix" src="Haut.png" title="Haut de page" height="110px" width="110px">
             </a>
 
-            <a class="pix" href="https://github.com/jean-derieppe">
-            <img src="GitCat.png" title="Lien vers GitHub" alt="img CatGit" height="110px" width="110px">
+            <a href="https://github.com/jean-derieppe">
+            <img class="pix" src="GitCat.png" title="Lien vers GitHub" alt="img CatGit" height="110px" width="110px">
             </a>
 
         </ul>
