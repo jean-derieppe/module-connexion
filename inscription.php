6e2b112e7c8e6df75052cfd08208717e59d19d55
jean-derieppe <jean.derieppe@laplateforme.io>
2022-12-11 15:22:25 +0100

    inscription OK

--- inscription.php
+++ inscription.php
@@ -1,9 +1,57 @@
 <?php
-   session_start();
-   include("header.php");
-   require_once("Connect.php");
-//    include('function/function.php');
+session_start();
+include("header.php");
+require_once("Connect.php");
+
+if (isset($_POST["submit"])){                
+    // parcour uniquement le $_post de submit ou tous ? et retourne un bolléen true ou false ? 
+    if (isset($_POST["name"]) && isset($_POST['surname']) && isset($_POST['login']) && isset($_POST['password'])){
+        $nom=$_POST["name"];
+        $prenom=$_POST["surname"];
+        $login=$_POST["login"];
+        $pass=$_POST["password"];
+        $repass=$_POST["repass"];
+        // variable qui définie les erreurs a afficher
+        $erreur = '';
+        //variable initié 
+        $user = 0;
+        // si $pass vaut $repass alors
+        if ($pass == $repass){
+            // Variable qui défini la requête SQL
+            $req_check_user = "SELECT `login` FROM utilisateurs";
+            // Variable qui prépare et éxecute la requête défini plus haut vers la base de donnée 
+            $req = $conn->query($req_check_user);
+
+            // retourne tous les résultats de la reqûete $req dans un tableau 
+            $result_user = $req->fetch_all();
+            // pour $x = 0 si le tableau retourne un résultat alors incrémenter $x de 1 pour chaque résultat correspondant
+            for($x = 0; isset($result_user[$x][0]); $x++){
+                // si le résultat retourné par le tableau correspond à $_POST login
+                if($result_user[$x][0] == $_POST['login']){
+                    $erreur = "<h1 class= 'erreur'>Login déja existant</h1>";
+                    // permet d'incrémenter $user si une correspondance est trouvé .
+                    $user ++;
+                }
+            }
+            // Si user vaut zéro alors créer compte
+            if($user == 0){
+            // créer la requête pour insérer dans utilisateurs, les valeurs login , prénom , nom et password)
+            $req = "INSERT INTO `utilisateurs`(`login`, `prenom`, `nom`, `password`) VALUES ('$login', '$prenom', '$nom', '$pass')";
+            // éxécution de la requête.
+            $create = $conn->query($req);
+            // Redirection vers la page connexion.php
+            header ('Location: connexion.php');
+            }
+    
+            }else{
+                $erreur = "<h1 class= 'erreur'>password non similaire</h1>";
+            }
+    }
+}
+
+
 ?>
+
 <html>
     <head>
         <html lang="fr">
@@ -37,51 +85,15 @@
                 <input type="password" name="repass" placeholder="Confirm password" required /><br>
                 <input type="submit" name="submit" value="S'inscrire" class="box-button"/>
             </form>
+
+        <?php
+            // si la variable $erreur existe alors echo
+            if(isset($erreur)){
+                echo "$erreur";
+            }
+        ?>
             <br><hr>
 
-<?php                                             
-    // parcour uniquement le $_post de submit ou tous ? et retourne un bolléen true ou false ? 
-    if (isset($_POST["name"]) && isset($_POST['surname']) && isset($_POST['login']) && isset($_POST['password'])){
-        $nom=$_POST["name"];
-        $prenom=$_POST["surname"];
-        $login=$_POST["login"];
-        $pass=$_POST["password"];
-        $repass=$_POST["repass"];
-    //    print_r($_POST);
-    $user = 0;
-        // si $pass vaut $repass alors
-        if ($pass == $repass){
-            // Variable qui défini la requête SQL
-            $req_check_user = "SELECT `login` FROM utilisateurs";
-            // Variable qui prépare et éxecute la requête défini plus haut vers la base de donnée 
-            $req = $conn->query($req_check_user);
-
-            // retourne tous les résultats de la reqûete $req dans un tableau 
-            $result_user = $req->fetch_all();
-            // pour $x = 0 si le tableau retourne un résultat alors incrémenter $x de 1 pour chaque résultat correspondant
-            for($x = 0; isset($result_user[$x][0]); $x++){
-                // si le résultat retourné par le tableau correspond à $_POST login
-                if($result_user[$x][0] == $_POST['login']){
-                    echo '<h1>Login déja existant</h1>';
-                    // permet d'incrémenter $user si une correspondance est trouvé .
-                    $user ++;
-                }
-            }
-        // Si user vaut zéro alors créer compte
-        if($user == 0){
-            // créer la requête pour insérer dans utilisateurs, les valeurs login , prénom , nom et password)
-            $req = "INSERT INTO `utilisateurs`(`login`, `prenom`, `nom`, `password`) VALUES ('$login', '$prenom', '$nom', '$pass')";
-            // éxécution de la requête.
-            $create = $conn->query($req);
-            // Redirection vers la page connexion.php
-            header ('Location: connexion.php');
-        }
-    
-        }else{
-            echo "<h1>password non similaire</h1>";
-        }
-    }
-?>
 
             <!--  Si connecté , masquer l'onglet inscription  -->
             <p><strong>Déjà inscrit? <a id="locate" href="connexion.php">Connectez-vous ici</a></strong></p>
