3fb7078634b584bdbe429a6a9e556fea2102416b
jean-derieppe <jean.derieppe@laplateforme.io>
2022-12-11 12:24:23 +0100

    connexion ok

--- connexion.php
+++ connexion.php
@@ -14,61 +14,58 @@
    </head>
 
    <body>
-      <div class="div1">
-
-
+      
+      <div>
+         <hr>
          <h1>Page de Connexion</h1>
 
-         <h2>Connectez-Vous :</h2>
-
       <!--    Formulaire de connexion -->
-
-         <form action="" method="post">
+      </div>
+      <div id="Formulaire1">
+         <form id="form" action="" method="post">
             <label><b>Login</b></label>
             <input type="text" placeholder="Enter your login" name="login" required>
-            <p>
+            
             <label><b>Mot de passe</b></label>
             <input type="password" placeholder="Enter your password" name="password" required>
-            </p>
             <input type="submit" name='submit' value='submit' >
          </form>
          <hr>
-            <p>Pas de compte ? <a href="inscription.php">Inscrivez-vous ici</a></p>
+         <p><strong>Pas de compte ?<a id="locate" href="inscription.php">Inscrivez-vous ici</a></strong></p>
          <hr>
-         </div>
+      </div>
    </body>
 
-
 <?php
+  $user=0;
 
-         //Requête pour chercher les données en bdd 
-         if (isset($_POST["submit"])){
-            $login=$_POST["login"];
-            $password=$_POST["password"];
-         
-            if($login && $password != 0 ){
-               $requete = "SELECT login,password FROM utilisateurs"; 
-               $exec_requete = $conn -> query($requete);
-               $reponse      = mysqli_fetch_array($exec_requete);
-               $count = $reponse['count(*)'];
+if (isset($_POST["login"]) && isset($_POST['password'])){
+   $login=$_POST["login"];
+   $pass=$_POST["password"];
 
-               if($count!=0){ // nom d'utilisateur correct
-                  $req = "SELECT login FROM utilisateurs where password = '".$password."'";
-                  $exec_req = $conn -> query($req);
-                  $reponse      = mysqli_fetch_array($exec_req);
-                  $password_hash = $reponse['password'];
-               
-               }else{
-                  echo "<h1>Login ou ( password ) incorrect</h1>";
-               }
-            
-            } else{
-               echo "<h1>( Login ) ou password incorrect</h1>";
-            }
+   if($_POST["login"] === 'admin' && $_POST["password"] === 'admin'){
+      header ('Location: admin.php');
+   }
+      else{
+      // $req est le chemin défini pour chercher un user ou le login et password correspond
+      $req = mysqli_query($conn, "SELECT * FROM utilisateurs WHERE login = '$login' AND password = '$pass' ");
+      // compte le nombre de ligne ou $login et $pass correspondent à la requete.
+      $result_ligne = mysqli_num_rows($req);
+      if($_POST["login"] === 'admin' && $_POST["password"] === 'admin'){
+         header ('Location: admin.php');
+      }
+      else{
+         if ($result_ligne > 0){
+            header ('Location: profil.php');
+            // header( 'location : profil.php ');
+         }else{
+            echo "<h1>compte inexistant ou erreur détecté</h1>";
+            // header(' location : inscription.php');
          }
+      }
+   }
+}
 
-mysqli_close($conn); 
-
+include("footer.php");
+mysqli_close($conn);
 ?>
-
-<?php Include ("Footer.php") ?>
